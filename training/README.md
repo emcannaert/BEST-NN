4342b7f2c8b519c4efbd622f4d463d6d67e3754b
Mark Samuel <msabbott@live.com>
2022-04-22 10:02:29 -0500

    Cleaned up working directory, rewrote scripts

--- training/README.md
+++ training/README.md
@@ -2,42 +2,86 @@
 
 The programs in this directory train the BEST.
 
-## Overview
-There are a few different training strategies that live in this directory.
+### Test Code Directory
 
-``johanTraining.py`` uses the shape-matching technique.
+The ``test`` directory contains scripts used to test different Scalers and Neural Net Archetectures for BEST. These are not normally necessary to train BEST, but could be of use to those who would like to modify BEST.
 
 ## Using the FermiLab GPUs
 
 BEST is a complicated network that takes a lot of processing to train, so we recommend training it using a GPU. 
-To use the GPU at FermiLab, set up an appropriate GPU environment. 
-Do NOT use the CMS environment.
+To use the GPU at FermiLab, set up an appropriate GPU environment. **Do NOT use the CMS environment.**
+[Learn more about the cmslpc gpus.](https://uscms.org/uscms_at_work/computing/setup/gpu.shtml)
+First, log into one of the three gpu nodes (cmslpcgpu1, cmslpcgpu2, or cmslpcgpu3). Then, set up a screen so your tests can safely run for a long time.
 
 ```bash
-/bin/bash --login
+ssh -Y USERNAME@cmslpcgpu3.fnal.gov
+screen
+bash -l
+```
+
+### mlenv0 Environment 
+
+The Basic Neural Net version of BEST runs on tensorflow 1.12. The (depreciated) mlenv0 environment is used to run this version of BEST. To set up: 
+
+```bash
 source /cvmfs/cms-lpc.opensciencegrid.org/sl7/gpu/Setup.sh
 source activate mlenv0
 ```
 
-## Training with Batch Generator pT flattening
-
-Our current method of training BEST uses a batch generator for pT flattening. This training is done in three steps on the LPC GPU.
-
-```bash
-python MakeFlatWeights.py
-python MakeStandardInputs.py -s all -hd </path/to/hdSamples/>
-python TrainWithGenerators.py
-```
-
-## Training with Shape-Matching
-
-The pT of the samples have been decorrelated (shape-matched) in the formatConverter step. Before the training, one must standardize the inputs for all file sets. Then one trains the network. Once done, one can evaluate the model on the test data. Thus the training and evaluation is done in three steps on the LPC GPU.
+### Singularity Containers
+The Point Cloud Transformer (PCT) runs on tensorflow 2.6. To set up a Docker environment with Singularity: 
 
 ```bash
-python MakeStandardInputs.py -s all -sf flattened -st train,validation,test
-python johanTraining.py
-python plotConfusionMatrix.py
+singularity run --nv --bind `readlink $HOME` --bind `readlink -f ${HOME}/nobackup/` --bind /uscms_data/d3/bonillaj/ --bind /cvmfs /cvmfs/unpacked.cern.ch/registry.hub.docker.com/fnallpc/fnallpc-docker:tensorflow-latest-gpu-singularity
 ```
+
+You must bind any directories that you want to access from within the singularity container. Here, ``/uscms_data/d3/bonillaj/`` is where our datasets are stored. There seems to be a bug with accessing the nobackup spaces, as the symbolic link uses ``uscms_data/d1/``. Using ``uscms_data/d3/`` to access the nobackup space seems to work for now. 
+
+**Do not bind your CWD.** It is recommended to make a symbolic link from your home directory to your working BEST directory, which will allow you to access your CWD from the virtual environment.
+
+```bash
+cd ~
+ln -s /uscms_data/d3/username/PATH_TO_CMSSW/CMSSW_10_6_27/src/BEST best
+```
+
+#### Version Control
+In case this Singularity Docker container changes names or is updated in the future, check the [fnallpc Docker Hub](https://hub.docker.com/r/fnallpc/fnallpc-docker/tags), where we use [this tag](https://hub.docker.com/layers/fnallpc-docker/fnallpc/fnallpc-docker/tensorflow-latest-gpu-singularity/images/sha256-708a3151db6c051f2ba88ed0497a5545ef9062a2b56f7e35583c45818c807fc6?context=explore) specifically. According to the tag page, this command will add the image if it is no longer available:
+
+```bash
+ADD file:e729fb032bd2f7cde20fb343da0cd358447e8b23028422c123944e8d0be660fa in / 
+```
+
+## Training Basic Neural Net BEST with Shape-Matching
+
+The pT of the samples have been decorrelated (shape-matched) in the formatConverter step. Before the training, one must standardize the inputs for all file sets. Then one trains and evaluates the network. There are two architectures available, oldBEST and nnBEST. These are fairly simple architectures, and can be used to test the BEST Input Variables before using the much more complex PCT. Options are available to choose the input data. Make sure that your mlenv0 environment is set up prior to standardizing or training BEST. If your h5 input sample files differ, you will need to update the lists "sampleTypes" and "samples" that appear throughout this directory and others.
+
+### Standardize Input Variables
+```bash
+# Options can be specified to tweak standardization. Use help flag for more information:
+python MakeStandardInputs.py --help
+```
+
+### Train oldBEST
+```bash
+# Many options can be specified to tweak training. Use help flag for more information:
+python oldBEST.py --help
+```
+
+### Train nnBEST
+```bash
+# Many options can be specified to tweak training. Use help flag for more information:
+python nnBEST.py --help
+```
+
+### Plotting Performance
+
+The performance of the models is plotted by the ``plotAll`` function within ``tools/functions.py``. The training scripts above will automatically call this plotting script. However, to plot the performance of an already existing, already trained model, one can add the ``-t`` flag to either training script above. This information is also available in the help message for those training scripts.
+
+### Plotting Input Variables
+
+The BEST input variables can be plotted from their respective h5 files using ``plotBESTInputs.py``. This is helpful in debugging potential issues with the input data.
+
+
 
 ## Warning About Functions in Python
 
--- training/nnBEST.py
+++ training/nnBEST.py
@@ -1,9 +1,9 @@
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# oldBEST.py //////////////////////////////////////////////////////////////////////
+# nnBEST.py ///////////////////////////////////////////////////////////////////////
 #==================================================================================
-# Author(s): Sam Abbott, Reyer Band, Johan S. Bonilla, Brendan Regnary  ///////////
+# Author(s): Sam Abbott, Reyer Band, Johan S. Bonilla, Brendan Regnary,  ////////////
 # This program trains BEST with flattened inputs //////////////////////////////////
-# This uses the original oldBEST NN architecture //////////////////////////////////
+# This uses the newBEST NN architecture ///////////////////////////////////////////
 #==================================================================================
 
 # user modules
@@ -15,7 +15,6 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg') #prevents opening displays, must use before pyplot
 import tensorflow as tf
-from sklearn.externals.joblib import dump
 
 # set up keras
 import argparse, os
@@ -25,6 +24,7 @@ from keras.models import Model
 from keras.layers import Input, Dense
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.models import load_model
+from sklearn.externals.joblib import load, dump
 
 # set up gpu environment
 from keras import backend as k
@@ -49,14 +49,20 @@ maxEvents = {"train":None,      "validation":None,   "test":None}
 # maxEvents = {"train":250000,  "validation":25000,  "test":None}
 # maxEvents = {"train":50000,   "validation":10000,  "test":None}
 
-def trainOldBEST(strings, pat, mask, dataDict):
-    print("Begin training oldBEST")
-
+def trainNNBEST(args, strings,mask, dataDict):
+    print("Begin training BEST")
+    
     # Unpack strings for readability
     modelFile = strings["modelFile"] 
     historyFile = strings["historyFile"] 
     # plotDir = strings["plotDir"] # not used in this function 
-    # suffix = strings["suffix"] # not used in this function 
+    suffix = strings["suffix"]
+
+    # Unpack relevant args
+    tol = float(args.tolerance)
+    pat = float(args.patience)
+    nodes = int(args.nodes)
+    modelType = args.modelType
 
     #==================================================================================
     # Train the Neural Network ////////////////////////////////////////////////////////
@@ -74,9 +80,9 @@ def trainOldBEST(strings, pat, mask, dataDict):
     combined = besModel.output
 
     # The network architecture consists of 3 hidden layers with 40 nodes in each layer using a rectified-linear activation function.
-    combLayer   = Dense(80, kernel_initializer="glorot_normal", activation="relu"   )(combined)
-    combLayer   = Dense(80, kernel_initializer="glorot_normal", activation="relu"   )(combLayer)
-    combLayer   = Dense(80, kernel_initializer="glorot_normal", activation="relu"   )(combLayer)
+    combLayer   = Dense(nodes, kernel_initializer="glorot_normal", activation="relu"   )(combined)
+    combLayer   = Dense(nodes, kernel_initializer="glorot_normal", activation="relu"   )(combLayer)
+    combLayer   = Dense(nodes, kernel_initializer="glorot_normal", activation="relu"   )(combLayer)
     outputModel = Dense( 6, kernel_initializer="glorot_normal", activation="softmax")(combLayer)
 
     # Compile the model
@@ -84,10 +90,10 @@ def trainOldBEST(strings, pat, mask, dataDict):
     myModel.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     print(myModel.summary() )
 
-    # Train the neural network
     # Early stopping
-    early_stopping = EarlyStopping(monitor='val_loss', min_delta=0.01, patience=pat, verbose=0, mode='auto')#, restore_best_weights=True,)
-    # early_stopping = EarlyStopping(monitor='val_loss', min_delta=0.001, patience=pat, verbose=0, mode='auto')#, restore_best_weights=True,)
+    # The smaller min_delta(tol) is, the longer the model with train, and the more accurate it will be.
+    # A value of min_delta(tol) that is too small could possibly lead to overtraining...
+    early_stopping = EarlyStopping(monitor='val_loss', min_delta=tol, patience=pat, verbose=1, mode='auto', restore_best_weights=True)
 
     # Model checkpoint callback
     # This saves the model architecture + parameters into a .pb file
@@ -96,15 +102,21 @@ def trainOldBEST(strings, pat, mask, dataDict):
                                         save_weights_only=False,
                                         period=1, mode='auto')
 
+    # Train the neural network
     history = myModel.fit( [dataDict["trainEvents"]], dataDict["trainTruth"], 
-                            validation_data = [[dataDict["validationEvents"]], dataDict["validationTruth"]],
+                            validation_data = [[dataDict["validationEvents"]], dataDict["validationTruth"]], 
                             batch_size=BatchSize, epochs=200, 
                             callbacks=[early_stopping, model_checkpoint],
                             shuffle = True, steps_per_epoch=None
-                         ) 
+                            )
 
     print("Trained the neural network!")
     del dataDict
+
+    # Record max accuracy
+    accIndex = np.argmax(history.history['val_acc'])
+    with open("Logs/" + modelType + "_accuracyLog.txt", "a") as f:
+        f.write("Ran " + suffix + ": acc = " + str(history.history['acc'][accIndex])[:6] + ",  Max val_acc = " + str(history.history['val_acc'][accIndex])[:6] + '\n')
 
     dump(history.history, historyFile)
 
@@ -119,29 +131,35 @@ if __name__ == "__main__":
                         default="models/",
                         help="Output Dir where models are saved [default: models/]")
     parser.add_argument('-mp','--maskPath', dest='maskPath',
-                        default = "../formatConverter/masks/oldBESTMask.txt",
-                        help="Path to mask file [default: ../formatConverter/masks/oldBESTMask.txt]")
+                        default = "../formatConverter/masks/BESTMask.txt",
+                        help="Path to mask file [default: ../formatConverter/masks/BESTMask.txt]")
     parser.add_argument('-sf','--suffix', dest='suffix',
                         default="",
                         help="Suffix, used to uniquely identify model [default: '']")
     parser.add_argument('-sc','--scale', dest='scale',
-                        default="standardized",
+                        default="standardized", # default="newBEST_Basic"
                         help="String used by MakeStandardInputs.py to name scaled data files [default: standardized]")
     parser.add_argument('-mt','--modelType', dest='modelType',
-                        default="oldBEST",
-                        help="Name of directory within models/ and plots/ [default: oldBEST]")
+                        default="nnBEST",
+                        help="Name of directory within models/ and plots/ [default: nnBEST]")
     parser.add_argument('-y','--year', dest='year',
                         default="2017",
                         help="Year of data taking to use [default: 2017]")
     parser.add_argument('-p','--patience', dest='patience',
                         default="20",
                         help="Number of Epochs to wait for improvement before EarlyStopping [default: 20]")
+    parser.add_argument('-tol','--tolerance', dest='tolerance',      
+                        default="0.01",
+                        help="Improvement tolerance for EarlyStopping; smaller tolerance means longer training [default: 0.01]")
+    parser.add_argument('-n','--nodes', dest='nodes',
+                        default="140",                     
+                        help="Number of nodes per hidden layer [default: 140]")
     parser.add_argument('-r','--replace', dest='replace',
                         action='store_true',
                         help="Boolean, use flag to overwrite current model and plots [default: False]")
     parser.add_argument('-t','--train', dest='train',
                         action='store_false',
-                        help="Boolean, use flag to load an already trained model and plot it. [default: True]")                         
+                        help="Boolean, use flag to load an already trained model and plot it. [default: True]")    
     args = parser.parse_args()
 
     # Generate appropriate helper strings, check dirs
@@ -150,18 +168,17 @@ if __name__ == "__main__":
     # Load Mask
     mask = tools.loadMask(args.maskPath)
 
-    # To skip training and plot the performance of an already trained model, use -t flag.
-    if args.train: # Train the model 
+    if args.train: # Run with -t to only plot the performance of the model
         # Load h5 Data, set up truth arrays
         dataDict = tools.loadH5Data(args, mask, sampleTypes, ["train", "validation"], maxEvents) 
 
         # Shuffle arrays
         tools.shuffleArray(dataDict)
 
-        # Train using oldBEST
-        trainOldBEST(strings, float(args.patience), mask, dataDict)
+        # Train using nnBEST
+        trainNNBEST(args, strings, mask, dataDict)
 
-    # Load test data for evaluating model performance
+    # Load test data, evaluate model performance
     dataDict = tools.loadH5Data(args, mask, sampleTypes, ["test"], maxEvents)
 
     # Make all performance plots
