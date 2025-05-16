1ba1352c6112d8cb06092ab0477b94d1e1c7aa82
Ethan <emcannaert@ucdavis.edu>
2025-05-16 17:57:11 -0500

    updating changes to flattener

--- formatConverter/sampleFlattener_2D_updated.py
+++ formatConverter/sampleFlattener_2D_updated.py
@@ -17,6 +17,7 @@ import numpy as np
 import numpy.ma as ma
 from sklearn.model_selection import train_test_split
 import pickle
+import random
 
 # years = ["2015","2016","2017","2018"]
 years = ["combine"]
@@ -36,38 +37,29 @@ setTypes = ["train"]
 
 def print_hist2d_matrix(hist, x_edges, y_edges):
     # Print column headers
-    print " " * 12,
+    print( " " * 12)
     for j in range(len(y_edges) - 1):
-        print "%7.1f-%-7.1f" % (y_edges[j], y_edges[j+1]),
-    print ""
+        print("%7.1f-%-7.1f" % (y_edges[j], y_edges[j+1]))
+    print("")
 
-    print "-" * (12 + (len(y_edges) - 1) * 15)
+    print("-" * (12 + (len(y_edges) - 1) * 15))
 
     # Loop over rows (x-axis bins)
     for i in range(len(x_edges) - 1):
         # Row label
-        print "%7.1f-%-7.1f |" % (x_edges[i], x_edges[i+1]),
+        print("%7.1f-%-7.1f |" % (x_edges[i], x_edges[i+1]))
         
         # Loop over columns (y-axis bins)
         for j in range(len(y_edges) - 1):
             count = hist[i][j]
-            print "%13.0f" % count,
-        print ""
+            print("%13.0f" % count)
+        print("")
 
 ## Loop over all files and keep/reject events in batches. One uses train_test_split to do the heavy lifting.
 ## The random state in the function ensures the same mask across the keys. Since the probabilities are binned in pt,
 ## one must loop over all pt bins and evaluate keep/reject for each.
 def flattenFile(keepProbs_2D, h5Dir, outDir, sampleTypes, year, setType, bins_SJ, bins_HT, userBatchSize, mass_type, args):
     print("Begin flattening for", setType)
-        
-    HT_max = args.rangeHighHT
-    HT_min = args.rangeLowHT
-
-    SJ_mass_max = args.rangeHighSJMass
-    SJ_mass_min = args.rangeLowSJMass
-
-    N_bins_HT = args.nBinsHT # represents HT
-    N_bins_SJ = args.nBinsSJMass  # represents SJ mass
 
     hist_to_count = []
 
@@ -80,7 +72,6 @@ def flattenFile(keepProbs_2D, h5Dir, outDir, sampleTypes, year, setType, bins_SJ
 
     # Loop over all samples and flatten each one by one
     for sample in sampleTypes:
-
 
         hist_2d, _, _ = np.histogram2d([], [], bins=[bins_HT, bins_SJ])
         hist_to_count.append( hist_2d  )
@@ -148,10 +139,8 @@ def flattenFile(keepProbs_2D, h5Dir, outDir, sampleTypes, year, setType, bins_SJ
                         saved_events_batch_HT.extend(sjMassData[combinedBool])
                         saved_events_batch_SJ_mass.extend(htData[combinedBool])
 
-
                         if result_old.shape[0] == 0:
                             continue
-                        
 
                         # Apply the probability to keep events
                         if prob < 1:
@@ -166,10 +155,10 @@ def flattenFile(keepProbs_2D, h5Dir, outDir, sampleTypes, year, setType, bins_SJ
                         if len(result) == 0:
                             continue
 
-                        hist_to_count2[sampleTypes.index(sample)][i][j] += len(result)
+                        hist_to_count2[sampleTypes.index(sample)][i][j]       += len(result)
                         all_events_to_count2[sampleTypes.index(sample)][i][j] += len(result_old)
-                        probs_to_count2[sampleTypes.index(sample)][i][j] = prob
-                        what_post_should_be[sampleTypes.index(sample)][i][j] += prob*len(result_old)
+                        probs_to_count2[sampleTypes.index(sample)][i][j]       = prob
+                        what_post_should_be[sampleTypes.index(sample)][i][j]  += prob*len(result_old)
 
 
                         # Store the kept data
@@ -177,14 +166,14 @@ def flattenFile(keepProbs_2D, h5Dir, outDir, sampleTypes, year, setType, bins_SJ
                             if myKey == "BES_vars":
                                 besData[myKey] = fOut.create_dataset(
                                     myKey, data=result, maxshape=(None, dsetShape[1]),
-                                    chunks=(dsetChunks[0], dsetChunks[1]), compression='lzf', shuffle=True
-                                )
+                                    chunks=(dsetChunks[0], dsetChunks[1]), compression='lzf', shuffle=True )
                         else:
                             besData[myKey].resize(besData[myKey].shape[0] + len(result), axis=0)
                             besData[myKey][-len(result):] = result
             counter += batchSize
             hist_2d_batch , _, _ = np.histogram2d(saved_events_batch_HT, saved_events_batch_SJ_mass, bins=[bins_HT, bins_SJ])  
             hist_to_count[sampleTypes.index(sample)] += hist_2d_batch.T
+
         print_hist2d_matrix(hist_to_count[sampleTypes.index(sample)],  bins_HT, bins_SJ)
 
     print("======== Final pre-flat signal histogram ==========")
@@ -209,9 +198,130 @@ def flattenFile(keepProbs_2D, h5Dir, outDir, sampleTypes, year, setType, bins_SJ
     print("========    BR PROBS  ==========")
     print_matrix(probs_to_count2[1],3,1)
 
-
     return
        
+
+
+
+
+## different versson of flattenFile that makes sure each bin has the EXACT same number of events 
+## VERY inefficienct, might not work with more than 35 bins
+
+def flattenFileNew(min_events, nEvents, h5Dir, outDir, sampleTypes, year, setType, bins_SJ, bins_HT, userBatchSize, mass_type, args):
+    print("Begin flattening for", setType)
+
+    hist_to_count = []
+
+    hist_to_count2 = []
+    all_events_to_count2 = []
+
+    # Loop over all samples and flatten each one by one
+    for sample in sampleTypes:
+
+        saved_events_batch_HT = []
+        saved_events_batch_SJ_mass = []
+
+        hist_2d, _, _ = np.histogram2d([], [], bins=[bins_HT, bins_SJ])
+        hist_to_count.append( hist_2d  )
+
+        hist_to_count2.append(np.zeros( (len(bins_HT)-1, len(bins_SJ)-1 ) ))
+        all_events_to_count2.append(np.zeros( (len(bins_HT)-1, len(bins_SJ)-1 ) ))
+
+        mass_str = mass_type + "_"
+        sample_str = sample + "_"
+
+        filePath = h5Dir + sample_str + 'Sample_' + mass_str + year + '_BESTinputs'
+        if setType == "":
+            filePath = filePath + ".h5"
+        else:
+            filePath = filePath + "_" + setType + ".h5"
+
+        fIn = h5py.File(filePath, 'r')
+        fOut = h5py.File(outDir + filePath.split('.')[-2].split('/')[-1] + "_flattened.h5", "w")
+        besData = {}
+        totalEvents = fIn[list(fIn.keys())[0]].shape[0]
+
+        sjMassData = np.array(fIn["BES_vars"][:, 88])
+        htData = np.array(fIn["BES_vars"][:, 111])
+
+        # Process each key in the dataset
+        for myKey in fIn.keys():
+            myKeyData = np.array(fIn[myKey][:, ...])
+            dsetShape = fIn[myKey].shape
+            dsetChunks = fIn[myKey].chunks
+            if dsetChunks is None:
+                dsetChunks = (min(1000, dsetShape[0]), dsetShape[1])  # Default chunk size
+            
+            # Loop over 2D bins (SJ mass and HT)
+            for i in range(0,len(bins_SJ)-1):
+                for j in range(0, len(bins_HT)-1):
+                    
+                    # Define the current bin ranges
+                    sjMassLow, sjMassHigh = bins_SJ[i], bins_SJ[i + 1]
+                    htLow, htHigh = bins_HT[j], bins_HT[j + 1]
+                    
+                    # Identify events in the current 2D bin
+                    sjMassBool = (sjMassLow < sjMassData) & (sjMassData < sjMassHigh)
+                    htBool = (htLow < htData) & (htData < htHigh)
+                    combinedBool = sjMassBool & htBool
+                        
+                    ## this replaces the original train_test_split
+                    result_old = myKeyData[combinedBool]
+
+
+                    ### get total number of events to be kept
+                    random.shuffle(result_old)   ## shuffle to get random selection of events
+
+
+                    #print("len(result_old) = %s"%(len(result_old)))
+
+                    ## get new results
+                    #### get the minimum number of events from this bin
+
+                    events_to_keep = int(  min(     nEvents[0][i][j],nEvents[1][i][j] )     )
+
+                    result = result_old[:events_to_keep]
+                    #print("For %s, i/j = %s/%s, the number of events is %s, will keep %s of these (nEvents[0][i][j] = %s, nEvents[1][i][j] = %s). In the end, kept %s events.  "%(sample, i,j, len(result_old), events_to_keep, nEvents[0][i][j], nEvents[1][i][j], len(result) ))
+
+                    #print("int(  min(     nEvents[0][i][j],nEvents[1][i][j] )     ) = %s"%(int(  min(     nEvents[0][i][j],nEvents[1][i][j] )     )))
+                    #print("int( nEvents[0][i][j]) = %s, int( nEvents[1][i][j]) = %s"%( int(nEvents[0][i][j]), int(nEvents[1][i][j])) )
+
+
+                    #assert len(result) == events_to_keep, "ERROR: len(result) == events_to_keep, len(result) = %s, events_to_keep = %s"%(len(result), events_to_keep)
+
+                    hist_to_count2[sampleTypes.index(sample)][i][j]       += len(result)
+                    all_events_to_count2[sampleTypes.index(sample)][i][j] += len(result_old)                    
+
+                    if len(result) == 0: continue
+
+                    # Store the kept data
+                    if myKey not in besData.keys():
+                        if myKey == "BES_vars":
+                            besData[myKey] = fOut.create_dataset(
+                                myKey, data=result, maxshape=(None, dsetShape[1]),
+                                chunks=(dsetChunks[0], dsetChunks[1]), compression='lzf', shuffle=True )
+                    else:
+                        besData[myKey].resize(besData[myKey].shape[0] + len(result), axis=0)
+                        besData[myKey][-len(result):] = result
+
+    print("======== Final pre-flat signal histogram ==========")
+    print_matrix(all_events_to_count2[0],0,1)
+    print("========    Final pre-flat BR histogram  ==========")
+    print_matrix(all_events_to_count2[1],0,1)
+
+
+    print("======== Final post-flat signal histogram ==========")
+    print_matrix(hist_to_count2[0],0,1)
+    print("========    Final post-flat BR histogram  ==========")
+    print_matrix(hist_to_count2[1],0,1)
+
+    return
+
+
+
+
+
+
 ## Plot samples in pt (or variable of choice) and return a list of probabilities for keeping events
 def getProbabilities(h5Dir, sampleTypes, year, setType, bins, binSize, maxRange, flattenIndex, mass_type):
     print("Begin making probabilities array")
@@ -282,17 +392,7 @@ def getProbabilities(h5Dir, sampleTypes, year, setType, bins, binSize, maxRange,
 ## return 2D probability to flatten wrt two vars (here HT and SJ mass)
 ### WARNING: this assumes only two categories: sig and BR. This is not meant for to be used for arbitrary categories (though could be adapted)
 
-def getProbabilities2D(h5Dir, sampleTypes, year, setType, mass_type,args):
-
-    ## init vars to use 
-    HT_max = args.rangeHighHT
-    HT_min = args.rangeLowHT
-
-    SJ_mass_max = args.rangeHighSJMass
-    SJ_mass_min = args.rangeLowSJMass
-
-    N_bins_HT = args.nBinsHT # represents HT
-    N_bins_SJ = args.nBinsSJMass  # represents SJ mass
+def getProbabilities2D(h5Dir, sampleTypes, year, setType, mass_type,args, bins_HT, bins_SJ):
 
     nEvents = []
     probs  = []
@@ -317,7 +417,7 @@ def getProbabilities2D(h5Dir, sampleTypes, year, setType, mass_type,args):
         SJ_mass_values= np.array(f["BES_vars"][...,SJ_mass_index])
         SJ_mass_values.astype(float)
         # Create 2D histogram for SJ mass and HT
-        hist2d, _, _ = np.histogram2d(HT_values, SJ_mass_values, bins=[N_bins_HT,N_bins_SJ], range=[[HT_min, HT_max], [SJ_mass_min, SJ_mass_max]])
+        hist2d, _, _ = np.histogram2d(HT_values, SJ_mass_values, bins=[bins_HT,bins_SJ])
         nEvents.append(hist2d.T)
     
     nEvents = np.array(nEvents) # this is a 2xNxM matrix (2x (NxM) for {sig,BR}x{HT}x{SJ mass})
@@ -333,7 +433,6 @@ def getProbabilities2D(h5Dir, sampleTypes, year, setType, mass_type,args):
 
     print("Calculating probabilities.")
     min_events = np.minimum(nEvents[0], nEvents[1])  # gives NxM array of the minimum events between samples
-    min_events = np.array(min_events).astype(float)
     probs.append(  min_events/nEvents[0] )
     probs.append(  min_events/nEvents[1] )
 
@@ -350,6 +449,48 @@ def getProbabilities2D(h5Dir, sampleTypes, year, setType, mass_type,args):
 
     return probs ##  [signal_probs, BR_probs]
 
+
+
+
+
+## return 2D array that gives the minimum number of events that between training categories in a given bin
+### 
+
+def getMinNEventsPerBin(h5Dir, sampleTypes, year, setType, mass_type,args, bins_HT, bins_SJ):
+    nEvents = []
+    probs  = []
+
+    HT_index      = 111 # change to the index of HT
+    SJ_mass_index = 88 # change to index of SJ mass
+
+    for sample in sampleTypes:
+        ### get sig N events
+        print("Getting events for %s."%sample)
+        filePath = h5Dir+ "%s_"%sample +'Sample_all_mass_'+year+'_BESTinputs'+"_"+setType+".h5"
+
+        print("Looking for file: %s" % filePath)
+        f = h5py.File(filePath, 'r')
+        if f: print("Found file: %s" % filePath)
+        else: 
+            print("ERROR finding file: %s" % filePath)
+            return
+
+        HT_values    = np.array(f["BES_vars"][...,HT_index])
+        SJ_mass_values= np.array(f["BES_vars"][...,SJ_mass_index])
+        # Create 2D histogram for SJ mass and HT
+        hist2d, _, _ = np.histogram2d(HT_values, SJ_mass_values, bins=[bins_HT,bins_SJ])
+        nEvents.append(hist2d.T)
+    
+    nEvents = np.array(nEvents) # this is a 2xNxM matrix (2x (NxM) for {sig,BR}x{HT}x{SJ mass})
+
+    min_events = np.minimum(nEvents[0], nEvents[1])  # gives NxM array of the minimum events between samples
+    print("======== MIN EVENTS ARE ============ ")
+    print_matrix(min_events,0,1)
+
+    return min_events, nEvents ##  [signal_probs, BR_probs]
+
+
+
 def print_matrix(matrix, precision=2, cell_width=6):
     """
     Pretty-prints a 2D list or NumPy array like a matrix in Python 2.
@@ -361,26 +502,16 @@ def print_matrix(matrix, precision=2, cell_width=6):
     """
     fmt_float = "%%%d.%df" % (cell_width, precision)
     for row in matrix:
-        formatted_row = " ".join(
-            fmt_float % elem if isinstance(elem, float)
-            else str(elem).rjust(cell_width)
-            for elem in row
-        )
-        print formatted_row
+        formatted_row = " ".join( fmt_float%elem if isinstance(elem, float) else str(elem).rjust(cell_width) for elem in row )
+        print(formatted_row)
+
+
+
+
 
 ## helper function for testing: write (to a pkl) the number of events in each 2D bin for both samples AFTER flattening
 def printPostFlatNEvents(h5Dir, sampleTypes, year, setType, mass_type,args, bins_HT, bins_SJ):
     
-    ## init vars to use 
-    HT_max = args.rangeHighHT
-    HT_min = args.rangeLowHT
-
-    SJ_mass_max = args.rangeHighSJMass
-    SJ_mass_min = args.rangeLowSJMass
-
-    N_bins_HT = args.nBinsHT # represents HT
-    N_bins_SJ = args.nBinsSJMass  # represents SJ mass
-
     nEvents = []
     probs  = []
 
@@ -389,7 +520,8 @@ def printPostFlatNEvents(h5Dir, sampleTypes, year, setType, mass_type,args, bins
 
     for sample in sampleTypes:
 
-        nEvents.append(np.zeros( (N_bins_HT, N_bins_SJ) ))
+        hist_2d, _, _ = np.histogram2d([], [], bins=[bins_HT, bins_SJ]) ## ERROR WARNING: could be a problem if HT and SJ mass have different number of bins
+        nEvents.append( hist_2d )
 
         ### get sig N events
         print("Getting events for %s."%sample)
@@ -422,7 +554,7 @@ def printPostFlatNEvents(h5Dir, sampleTypes, year, setType, mass_type,args, bins
             SJ_mass_values = np.array(fIn["BES_vars"][counter:counter + batchSize, 88])
             HT_values = np.array(fIn["BES_vars"][counter:counter + batchSize, 111])
 
-            hist2d, _, _ = np.histogram2d( HT_values, SJ_mass_values, bins=[N_bins_HT,N_bins_SJ], range=[[HT_min, HT_max], [SJ_mass_min, SJ_mass_max]])
+            hist2d, _, _ = np.histogram2d( HT_values, SJ_mass_values, bins=[bins_HT,bins_SJ])
             
             nEvents[sampleTypes.index(sample)] += hist2d.T
 
@@ -433,11 +565,6 @@ def printPostFlatNEvents(h5Dir, sampleTypes, year, setType, mass_type,args, bins
             batch_num+=1
             counter += batchSize
 
-
-        """# Create 2D histogram for SJ mass and HT
-        hist2d, _, _ = np.histogram2d(HT_values, SJ_mass_values, bins=[N_bins_HT,N_bins_SJ], range=[[HT_min, HT_max], [SJ_mass_min, SJ_mass_max]])
-        nEvents.append(hist2d)"""
-    
     nEvents = np.array(nEvents) # this is a 2xNxM matrix (2x (NxM) for {sig,BR}x{HT}x{SJ mass})
     nEvents = nEvents.astype(float)
 
@@ -445,10 +572,10 @@ def printPostFlatNEvents(h5Dir, sampleTypes, year, setType, mass_type,args, bins
     print("============ DONE ============")
     print("==============================")
 
-    print("Number of POST-FLAT signal events: ")
+    print("Number of POST-FLAT signal events AFTER REOPENING FILE: ")
     print_matrix(nEvents[0],0,1)
 
-    print("Number of POST-FLAT background events: ")
+    print("Number of POST-FLAT background events AFTER REOPENING FILE: ")
     print_matrix(nEvents[1],0,1)
 
     return
@@ -505,7 +632,7 @@ if __name__ == "__main__":
                         dest='rangeLowHT',
                         type=float,
                         # default=0)
-                        default=1400)
+                        default=1500)
     parser.add_argument('--rangeHighHT',
                         dest='rangeHighHT',
                         type=float,
@@ -557,8 +684,11 @@ if __name__ == "__main__":
         for mass_type in mass_types:
             for year in years:
                 for setType in setTypes:
-                    keepnEvents2D = getProbabilities2D(args.h5Dir, sampleTypes, year, setType, mass_type,args)
-                    flattenFile(keepnEvents2D, args.h5Dir, args.outDir, sampleTypes, year, setType, bins_SJ, bins_HT, args.batchSize, mass_type, args)
+                    #keepnEvents2D = getProbabilities2D(args.h5Dir, sampleTypes, year, setType, mass_type,args)
+                    #flattenFile(keepnEvents2D, args.h5Dir, args.outDir, sampleTypes, year, setType, bins_SJ, bins_HT, args.batchSize, mass_type, args)
+
+                    min_events_2d, nEvents_2d = getMinNEventsPerBin(args.h5Dir, sampleTypes, year, setType, mass_type,args,bins_HT,bins_SJ)
+                    flattenFileNew(min_events_2d, nEvents_2d, args.h5Dir, args.outDir, sampleTypes, year, setType, bins_SJ, bins_HT, args.batchSize, mass_type, args)
                     printPostFlatNEvents(args.h5Dir, sampleTypes, year, setType, mass_type,args, bins_HT, bins_SJ)
 
 
